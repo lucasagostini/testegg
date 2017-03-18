b856ddd40f97b7f839193d17126738ff23dd5c04
lucasagostini <lbagostini@inf.ufpel.edu.br>
2017-03-18 19:54:12 -0300

    Usando todos como cripto, funcao removendo add

--- teste.c
+++ teste.c
@@ -5,12 +5,25 @@
 #include "cryfs.h"
 #include "simpletest.h"
 
+cry_desc_t * openfs() {
+	initfs("dc/cripto",100000);
+	return cry_openfs("dc/cripto");
+}
+
+int removendo(){
+	remove("dc/cripto");
+	remove("dc/cripto.log");
+	return 0;
+}
+
 void teste1() {
+	removendo();
 	DESCRIBE("1-Testa criacao sistema de arquivos");
 	WHEN("Nenhum sistema de arquivo ainda existe");
 	IF("Um sistema Criado");
 	THEN("A funcao retornara SUCESSO");
-	isEqual(initfs("dc/cripto1",19),SUCESSO);
+	isEqual(initfs("dc/cripto",19),SUCESSO);
+	removendo();
 }
 
 void teste2() {
@@ -18,8 +31,9 @@ void teste2() {
 	WHEN("Dois sistemas sao criados com mesmo nome");
 	IF("Dois Sistemas de Arquivos com mesmo nome");
 	THEN("A funcao retorna FALHA");
-	initfs("dc/cripto2",200);
-	isEqual(initfs("dc/cripto2",200),FALHA);
+	initfs("dc/cripto",200);
+	isEqual(initfs("dc/cripto",200),FALHA);
+	removendo();
 }
 
 void teste3() {
@@ -27,7 +41,8 @@ void teste3() {
 	WHEN("Tamanho e menor que o minimo");
 	IF("Numero de blocos menor que 19");
 	THEN("O sistema nao pode ser criado");
-	isEqual(initfs("dc/cripto3",18),FALHA);
+	isEqual(initfs("dc/cripto",18),FALHA);
+	removendo();
 }
 
 void teste4() {
@@ -35,8 +50,9 @@ void teste4() {
 	WHEN("Um sistema e aberto");
 	IF("O sistema ja existe");
 	THEN("A funcao retorna sucesso");
-	initfs("dc/cripto4",19);
-	isNotNull(cry_openfs("dc/cripto4"));
+	initfs("dc/cripto",19);
+	isNotNull(cry_openfs("dc/cripto"));
+	removendo();
 }
 
 void teste5() {
@@ -44,21 +60,20 @@ void teste5() {
 	WHEN("Nao existe nenhum Sistema");
 	IF("Sistema nao foi criado");
 	THEN("A funcao retorna falha");
-	isNull(cry_openfs("dc/cripto5"));
+	isNull(cry_openfs("dc/cripto"));
+	removendo();
 }
 
-cry_desc_t * openfs(char* name) {
-	initfs(name,100000);
-	return cry_openfs(name);
-}
+
 
 void teste6() {
 	DESCRIBE("6-Testa a abertura de um ou mais arquivos");
 	WHEN("Sistema de arquivos ja existe, mas o arquivo nao");
 	IF("Arquivo nao existe e atributo eh de LEITURAESCRITA");
 	THEN("O arquivo deve ser criado e a funcao retorna o file handler");
-	cry_desc_t * fs = openfs("dc/cripto6");
+	cry_desc_t * fs = openfs();
 	isNotEqual(cry_open(fs, "arquivo1", LEITURAESCRITA, 0), 0);
+	removendo();
 }
 
 void teste7() {
@@ -66,8 +81,9 @@ void teste7() {
 	WHEN("Um mesmo arquivo eh aberto mais de uma vez");
 	IF("Arquivo ja esta aberto");
 	THEN("O File Handler retornado deve ser o do arquivo ja aberto");
-	cry_desc_t * fs = openfs("dc/cripto7");
+	cry_desc_t * fs = openfs();
 	isEqual(cry_open(fs, "arquivo1", LEITURAESCRITA, 0), cry_open(fs, "arquivo1", LEITURAESCRITA, 0));
+	removendo();
 }
 
 void teste8() {
@@ -75,8 +91,9 @@ void teste8() {
 	WHEN("Dois arquivos sao criados e abertos");
 	IF("Dois arquivos diferentes sao abertos");
 	THEN("A funcao deve retornar diferentes file handlers");
-	cry_desc_t * fs = openfs("dc/cripto8");
+	cry_desc_t * fs = openfs();
 	isNotEqual(cry_open(fs, "arquivo1", LEITURAESCRITA, 0), cry_open(fs, "arquivo2", LEITURAESCRITA, 0));
+	removendo();
 }
 
 void teste9() {
@@ -84,8 +101,9 @@ void teste9() {
 	WHEN("O acesso e de LEITURA");
 	IF("Acesso eh LEITURA e arquivo nao existe");
 	THEN("A funcao deve retornar FALHA");
-	cry_desc_t * fs = openfs("dc/cripto9");
+	cry_desc_t * fs = openfs();
 	isEqual(cry_open(fs, "arquivo9", LEITURA, 0), 0);
+	removendo();
 }
 
 void teste10() {
@@ -93,9 +111,10 @@ void teste10() {
 	WHEN("O File Handler eh passado como parametro");
 	IF("Arquivo esta aberto");
 	THEN("A funcao deve retornar SUCESSO");
-	cry_desc_t * fs = openfs("dc/cripto10");
+	cry_desc_t * fs = openfs();
 	int indice = cry_open(fs, "arquivo1", LEITURAESCRITA, 0);
 	isEqual(cry_close(indice), 1);
+	removendo();
 }
 
 void teste11() {
@@ -103,11 +122,12 @@ void teste11() {
 	WHEN("O File Handler eh passado como parametro");
 	IF("Arquivo 1 esta aberto e 2 fechado");
 	THEN("A funcao deve retornar SUCESSO");
-	cry_desc_t * fs = openfs("dc/cripto11");
+	cry_desc_t * fs = openfs();
 	int indice1 = cry_open(fs, "arquivo1", LEITURAESCRITA, 0);
 	cry_open(fs, "arquivo2", LEITURAESCRITA, 0);
 	cry_close(indice1);
 	isEqual(cry_open(fs, "arquivo3", LEITURAESCRITA, 0), 1);
+	removendo();
 }
 
 void teste12() {
@@ -115,7 +135,7 @@ void teste12() {
 	WHEN("Nao tem nenhum arquivo aberto");
 	IF("256 arquivos sao abertos");
 	THEN("A funcao deve retornar sucesso");
-	cry_desc_t * fs = openfs("dc/cripto12");
+	cry_desc_t * fs = openfs("dc/cripto2");
 	char str[12];
 	for (int i = 1; i <= 256; i++) {
 		sprintf(str, "%d", i);
@@ -124,6 +144,7 @@ void teste12() {
 		}
 	}
 	isEqual(1, 1);
+	removendo();
 }
 
 void teste13() {
@@ -131,9 +152,10 @@ void teste13() {
 	WHEN("Arquivo esta aberto");
 	IF("Temos espaco para escrever");
 	THEN("A funcao escreve e retorna sucesso");
-	cry_desc_t * fs = openfs("dc/cripto13");
+	cry_desc_t * fs = openfs();
 	int arquivo = cry_open(fs, "arquivo1", LEITURAESCRITA, 0);
 	isEqual(cry_write(arquivo, 3, "abc"), SUCESSO);
+	removendo();
 }
 
 void teste14() {
@@ -141,10 +163,11 @@ void teste14() {
 	WHEN("Arquivo esta fechado");
 	IF("Arquivo fechado");
 	THEN("A funcao retorna falha");
-	cry_desc_t * fs = openfs("dc/cripto14");
+	cry_desc_t * fs = openfs();
 	int arquivo = cry_open(fs, "arquivo1", LEITURAESCRITA, 0);
 	cry_close(arquivo);
 	isEqual(cry_write(arquivo, 3, "abc"), FALHA);
+	removendo();
 }
 
 void teste15() {
@@ -152,11 +175,12 @@ void teste15() {
 	WHEN("Quando o arquivo eh fechado e depois reaberto");
 	IF("Arquivo esta aberto");
 	THEN("A funcao deve retornar SUCESSO");
-	cry_desc_t * fs = openfs("dc/cripto15");
+	cry_desc_t * fs = openfs();
 	int arquivo = cry_open(fs, "arquivo1", LEITURAESCRITA, 0);
 	cry_close(arquivo);
 	arquivo = cry_open(fs, "arquivo1", LEITURA, 0);
 	isEqual(cry_write(arquivo, 3, "abc"), FALHA);
+	removendo();
 }
 
 void teste16() {
@@ -164,10 +188,11 @@ void teste16() {
 	WHEN("Ele esta vazio");
 	IF("Nao tem nada escrito");
 	THEN("A funcao nao pode ler nada, retorna falha");
-	cry_desc_t * fs = openfs("dc/cripto16");
+	cry_desc_t * fs = openfs();
 	int arquivo = cry_open(fs, "arquivo1", ESCRITA, 0);
 	char out[100];
 	isEqual(cry_read(arquivo, 100, out), FALHA);
+	removendo();
 }
 
 void teste17() {
@@ -175,13 +200,14 @@ void teste17() {
 	WHEN("Ele possui dados escritos");
 	IF("O que foi lido eh igual ao que foi escrito");
 	THEN("A funcao deve retornar sucesso");
-	cry_desc_t * fs = openfs("dc/cripto17");
+	cry_desc_t * fs = openfs();
 	int arquivo = cry_open(fs, "arquivo1", LEITURAESCRITA, 0);
 	cry_write(arquivo, 7, "teste17");
 	char out[7];
 	int lidos = cry_read(arquivo, 7, out);
 	isEqual(lidos, 7);
 	isEqual(strncmp(out, "teste17", lidos), 0);
+	removendo();
 }
 
 void teste18() {
@@ -189,7 +215,7 @@ void teste18() {
 	WHEN("Temos dados escritos no arquivo");
 	IF("Damos seek para a posicao 2 do arquivo");
 	THEN("A leitura deve ser feita a partir da segunda posicao");
-	cry_desc_t * fs = openfs("dc/cripto18");
+	cry_desc_t * fs = openfs();
 	int arquivo = cry_open(fs, "arquivo1", LEITURAESCRITA, 0);
 	cry_write(arquivo, 7, "teste18");
 	char out[5];
@@ -197,6 +223,7 @@ void teste18() {
 	int lidos = cry_read(arquivo, 5, out);
 	isEqual(lidos, 5);
 	isEqual(strncmp(out, "ste18", lidos), 0);
+	removendo();
 }
 
 void teste19() {
@@ -204,10 +231,11 @@ void teste19() {
 	WHEN("Escrevemos 7 char em um arquivo");
 	IF("Damos seek para a posicao 7");
 	THEN("A funcao deve retornar sucesso");
-	cry_desc_t * fs = openfs("dc/cripto19");
+	cry_desc_t * fs = openfs();
 	int arquivo = cry_open(fs, "arquivo1", LEITURAESCRITA, 0);
 	cry_write(arquivo, 7, "teste19");
 	isEqual(cry_seek(arquivo, 7), SUCESSO);
+	removendo();
 }
 
 void teste20() {
@@ -215,10 +243,11 @@ void teste20() {
 	WHEN("Escrevemos 7 char em um arquivo");
 	IF("Damos seek para a posicao 8");
 	THEN("A funcao deve retornar falha");
-	cry_desc_t * fs = openfs("dc/cripto20");
+	cry_desc_t * fs = openfs();
 	int arquivo = cry_open(fs, "arquivo1", LEITURAESCRITA, 0);
 	cry_write(arquivo, 7, "teste20");
 	isEqual(cry_seek(arquivo, 8), FALHA);
+	removendo();
 }
 
 void teste21() {
@@ -226,12 +255,13 @@ void teste21() {
 	WHEN("Um arquivo existe com dados dentro");
 	IF("Mandamos deletar ele e depois recriar");
 	THEN("O arquivo deve estar vazio, entao o seek(1) deve retornar falha");
-	cry_desc_t * fs = openfs("dc/cripto21");
+	cry_desc_t * fs = openfs();
 	int arquivo = cry_open(fs, "arquivo1", LEITURAESCRITA, 0);
 	cry_write(arquivo, 7, "teste21");
 	cry_delete(arquivo);
 	arquivo = cry_open(fs, "arquivo1", LEITURAESCRITA, 0);
 	isEqual(cry_seek(arquivo, 1), FALHA);
+	removendo();
 }
 
 void teste22() {
@@ -239,12 +269,13 @@ void teste22() {
 	WHEN("Um arquivo existe com dados dentro");
 	IF("Mandamos deletar ele e depois abrir em modo leitura");
 	THEN("A abertura deve retornar falha pois o arquivo nao existe e o modo eh leitura");
-	cry_desc_t * fs = openfs("dc/cripto22");
+	cry_desc_t * fs = openfs();
 	int arquivo = cry_open(fs, "arquivo1", LEITURAESCRITA, 0);
 	cry_write(arquivo, 7, "teste22");
 	cry_delete(arquivo);
 	arquivo = cry_open(fs, "arquivo1", LEITURA, 0);
 	isEqual(arquivo, FALHA);
+	removendo();
 }
 
 void teste23() {
@@ -252,10 +283,11 @@ void teste23() {
 	WHEN("O File Handler eh passado como parametro");
 	IF("Arquivo existe");
 	THEN("A funcao deve retornar o tempo de criacao do mesmo");
-	cry_desc_t * fs = openfs("dc/cripto23");
+	cry_desc_t * fs = openfs();
 	int indice = cry_open(fs, "arquivo1", LEITURAESCRITA, 0);
 	isNotEqual(cry_creation(indice), 0);
 	isEqual(cry_last_modified(indice), 0);
+	removendo();
 }
 
 void teste24() {
@@ -263,10 +295,11 @@ void teste24() {
 	WHEN("O File Handler eh passado como parametro");
 	IF("Arquivo existe");
 	THEN("A funcao deve retornar o tempo de criacao do mesmo");
-	cry_desc_t * fs = openfs("dc/cripto24");
+	cry_desc_t * fs = openfs();
 	int indice = cry_open(fs, "arquivo1", LEITURAESCRITA, 0);
 	isNotEqual(cry_accessed(indice), 0);
 	isEqual(cry_last_modified(indice), 0);
+	removendo();
 }
 
 void teste25() {
@@ -274,10 +307,11 @@ void teste25() {
 	WHEN("O File Handler eh passado como parametro");
 	IF("Arquivo existe");
 	THEN("A funcao deve retornar o tempo de modificacao do mesmo");
-	cry_desc_t * fs = openfs("dc/cripto25");
+	cry_desc_t * fs = openfs();
 	int indice = cry_open(fs, "arquivo1", LEITURAESCRITA, 0);
 	cry_write(indice, 7, "teste25");
 	isNotEqual(cry_last_modified(indice), 0);
+	removendo();
 }
 
 void teste26() {
@@ -285,7 +319,7 @@ void teste26() {
 	WHEN("Abrimos um mesmo arquivo com duas chaves diferentes");
 	IF("Escrevermos com uma chave e depois lermos usando chaves diferentes");
 	THEN("As strings nao podem ser iguais");
-	cry_desc_t * fs = openfs("dc/cripto26");
+	cry_desc_t * fs = openfs();
 	int arquivo1 = cry_open(fs, "arquivo1", LEITURAESCRITA, 2);
 	cry_write(arquivo1, 7, "teste26");
 	char out1[7],out2[7];
@@ -295,19 +329,21 @@ void teste26() {
 	lidos = cry_read(arquivo2, 7, out1);
 	isEqual(lidos, 7);
 	isNotEqual(strncmp(out1, out2, lidos), 0);
+	removendo();
 }
 void teste27() {
 	DESCRIBE("27-Testa leitura de um arquivo com uma chave");
 	WHEN("Abrimos um arquivo usando somente uma chave");
 	IF("Escrevermos com uma chave e depois lermos com a mesma chave");
 	THEN("As strings devem ser iguais");
-	cry_desc_t * fs = openfs("dc/cripto27");
+	cry_desc_t * fs = openfs();
 	int arquivo = cry_open(fs, "arquivo1", LEITURAESCRITA, 2);
 	cry_write(arquivo, 7, "teste27");
 	char out[7];
 	int lidos = cry_read(arquivo, 7, out);
 	isEqual(lidos, 7);
 	isEqual(strncmp(out, "teste27", lidos), 0);
+	removendo();
 }
 
 void teste28() {
@@ -315,7 +351,7 @@ void teste28() {
 	WHEN("Abrimos um mesmo arquivo com duas chaves diferentes");
 	IF("Escrevermos com chaves diferentes e depois lermos usando a chave certa");
 	THEN("As strings devem fazer sentido e serem iguais");
-	cry_desc_t * fs = openfs("dc/cripto28");
+	cry_desc_t * fs = openfs();
 	char out1[7],out2[7];
 	int arquivo1 = cry_open(fs, "arquivo1", LEITURAESCRITA, 2);
 	cry_write(arquivo1, 7, "teste28");
@@ -329,6 +365,7 @@ void teste28() {
 	isEqual(lidos2, 7);
 	cry_close(arquivo2);
 	isEqual(strncmp(out1, out2, lidos2), 0);
+	removendo();
 }
 
 void teste_final() {
@@ -336,7 +373,7 @@ void teste_final() {
 	WHEN("Executamos todas as funcoes");
 	IF("Todas funcionarem corretamente");
 	THEN("O retorno deve ser SUCESSO");
-	cry_desc_t * fs = openfs("dc/cripto");
+	cry_desc_t * fs = openfs();
 	char out1[7],out2[7];
 	int arquivo1 = cry_open(fs, "arquivo1", LEITURAESCRITA, 2);
 	cry_write(arquivo1, 7, "teste28");
@@ -354,8 +391,6 @@ void teste_final() {
 	isGreaterThan(cry_delete(arquivo2),0);
 }
 int main() {
-	int tamanho=sizeof(descritor_t)*256/4096;
-	printf("Tamanho_blocos = %d\n",tamanho);
 	teste1();
 	teste2();
 	teste3();
